9e69d66d85f838edf4889c01d9bdace157b3879a
Sehyeon Youn <dev.younlab@gmail.com>
2018-11-08 00:25:01 +0900

    [UPDATE] ajax

--- nomad-coders-react-basic/react-js-basic/src/App.js
+++ nomad-coders-react-basic/react-js-basic/src/App.js
@@ -2,29 +2,67 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitle = [
-  "Matrix",
-  "Full Metal Jackert",
-  "Old Boy",
-  "Star Wars",
-]
-
-const movieImage = [
-  "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fclm.or.kr%2Ftechnote7%2Fdata%2Fboard%2Fcwvmovie%2Ffile%2F1%2Fe65e6e3f_b445200.jpg&f=1",
-  "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimgmovie.naver.com%2Fmdi%2Fmi%2F0116%2FA1671-00.jpg&f=1",
-  "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fenter361.files.wordpress.com%2F2011%2F10%2Faaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.jpg&f=1",
-  "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fmoonhak.co.kr%2Fhome%2Fwp-content%2Fuploads%2Fbookcover%2F%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88-%EB%85%B8%EB%B8%945_%ED%91%9C1_web.jpg&f=1"
-
-]
 
 class App extends Component {
+  state = {}
+
+  componentDidMount(){
+    this._getMovies();
+  }
+  _renderMovies = ()=>{
+    const movies = this.state.movies.map(movie =>{
+      console.log(movie);
+      return <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+        key={movie.id}/>
+    })
+
+    return movies
+  }
+
+  _getMovies = async () =>{
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+  _callApi = () =>{
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+
+  _loading_Icon = () =>{
+    
+    return (
+    <div>
+      <svg width="200" height="200">
+        <rect id="square13" x="38%" y="40%" width="20" height="20" style={{fill:"black"}} />
+        <rect id="square14" x="50%" y="40%" width="20" height="20" style={{fill:"black"}}  />
+        <rect id="square15" x="62%" y="40%" width="20" height="20" style={{fill:"black"}}  />
+        <rect id="square16" x="38%" y="52%" width="20" height="20" style={{fill:"black"}}  />
+        <rect id="square17" x="50%" y="52%" width="20" height="20" style={{fill:"black"}}  />
+        <rect id="square18" x="62%" y="52%" width="20" height="20" style={{fill:"black"}}  />
+        <rect id="square19" x="38%" y="64%" width="20" height="20" style={{fill:"black"}}  />
+        <rect id="square20" x="50%" y="64%" width="20" height="20" style={{fill:"black"}}  />
+        <rect id="square21" x="62%" y="64%" width="20" height="20" style={{fill:"black"}}  />
+      </svg>
+    </div>
+    )
+
+  }
+
+
   render() {
     return (
       <div className='App'>
-        <Movie title={movieTitle[0]} image={movieImage[0]}/>
-        <Movie title={movieTitle[1]} image={movieImage[1]}/>
-        <Movie title={movieTitle[2]} image={movieImage[2]}/>
-        <Movie title={movieTitle[3]} image={movieImage[3]}/>
+        {/* {JSON.stringify(this.state)} */}
+        {this.state.movies ? this._renderMovies() : this._loading_Icon()}
       </div>
     );
   }
--- nomad-coders-react-basic/react-js-basic/src/Movie.js
+++ nomad-coders-react-basic/react-js-basic/src/Movie.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Movie.css';
+import PropTypes from 'prop-types';
 
-class Movie extends Component {
-    render() {
-        console.log(this.props);
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <MoviePoster image={this.props.image} title={this.props.title}/>
 
+function Movie({title, poster, genres, synopsis}){
+    return (
+        <div className="Movie">
+        <div className="Movie-wrap">
+        <div className="Movie-Columns">
+                <MoviePoster poster={poster} title={title}/>
             </div>
-        );
-    }
+            <div className="Movie-Columns">
+                <h1>{title}</h1>
+                <div className="Movie-Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+                </div>
+                <p className="Movie-Synopsis">{synopsis}</p>
+            </div>
+        </div>
+
+            
+        </div>
+    );
 }
 
-class MoviePoster extends Component{
-    render(){
-        return(
-            <img src={this.props.image} alt={this.props.title}/>
-        )
-    }
+function MoviePoster({poster, title}){
+    return(
+        <img src={poster} alt={title} title={title} className="Movie-Poster"/>
+    );
+}
+
+function MovieGenre({genre}){
+    return(
+        <span className="Movie-Genre">{genre} </span>
+    )
+}
+
+Movie.propTypes = {
+    poster:PropTypes.string.isRequired,
+    title:PropTypes.string.isRequired,
+    genres:PropTypes.array.isRequired,
+    synopsis:PropTypes.string.isRequired,
+}
+
+MoviePoster.propTypes = {
+    poster:PropTypes.string.isRequired,
+    title:PropTypes.string.isRequired,
+}
+
+MovieGenre.propTypes = {
+    genre:PropTypes.string.isRequired
 }
 
 export default Movie;
