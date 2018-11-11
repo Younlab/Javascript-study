6099475d028b1292d0c8e129d014d57c8a8c7dda
Sehyeon Youn <dev.younlab@gmail.com>
2018-11-11 14:33:21 +0900

    [UPDATE] const, let 의 정의;

--- nomad-coders-vanillaJavascript/exemple-01/js/index.js
+++ nomad-coders-vanillaJavascript/exemple-01/js/index.js
@@ -1,1 +1,6 @@
-console.log(alert('Im Working. Im JS. Im Beautiful. Im worth it'))
+// const 는 상수, 즉 변하지 않는 수로, 나중에 변수를 update 할 수 없도록 막는다.
+const a = 221;
+
+// let 는 변수, 즉 변할 수 있는 수로 const 와는 다르게 data 를 update 할 수 있다.
+let b = a -5;
+console.log(b);
