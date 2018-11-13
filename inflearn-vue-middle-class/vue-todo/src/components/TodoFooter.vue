f8c4b37fe9762aec4078c880179375e3f1a804a9
Sehyeon Youn <dev.younlab@gmail.com>
2018-11-13 18:25:01 +0900

    [UPDATE] footer component clear all btn

--- inflearn-vue-middle-class/vue-todo/src/components/TodoFooter.vue
+++ inflearn-vue-middle-class/vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,32 @@
 <template>
-    <div>
-        footer
-    </div>
+    <footer class="clearAllContainer">
+        <span @click="clearTodo" class="clearAllBtn">
+            Clear All
+        </span>
+    </footer>
 </template>
 
 <script>
 export default {
-
+    methods:{
+        clearTodo:function(){
+            localStorage.clear();
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+    .clearAllContainer{
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+    .clearAllBtn{
+        color: #e20303;
+        display: block;
+    }
 </style>
