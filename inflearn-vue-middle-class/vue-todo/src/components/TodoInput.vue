3f45a967a2cf2d7a7284560c2ddbafec3f8bafab
Sehyeon Youn <dev.younlab@gmail.com>
2018-11-14 17:48:13 +0900

    [UPDATE] modal 과 slot 에 대하여

--- inflearn-vue-middle-class/vue-todo/src/components/TodoInput.vue
+++ inflearn-vue-middle-class/vue-todo/src/components/TodoInput.vue
@@ -4,14 +4,28 @@
         <span class="addContainer" @click="addTodo">
             <i class="fas fa-plus addBtn"></i>
         </span>
+        <modal v-if="showModal" @close="showModal = false">
+            <!--
+            you can use custom content here to overwrite
+            default content
+            -->
+            <h3 slot="header">경고!</h3>
+            <div slot="body">
+                <p>아무런 값도 입력하지 않았습니다.</p>
+                <p>값이 입력되지 않으면 Todo가 추가되지 않습니다.</p>
+            </div>
+            <p slot="footer">copylight by Todo List</p>
+        </modal>
     </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue';
 export default {
     data:function(){
         return {
-            newTodoItem:""
+            newTodoItem:"",
+            showModal:false,
         }
     },
     methods:{
@@ -19,12 +33,17 @@ export default {
             if(this.newTodoItem !== ''){
             this.$emit('addTodoItem', this.newTodoItem)
             this.clearInput();
+            } else {
+                this.showModal = !this.showModal
             }
 
         },
         clearInput:function(){
             this.newTodoItem = '';
         }
+    },
+    components:{
+        Modal
     }
 }
 </script>
