ec83dd3b543a840709c8155719d3a3e2fb1539dd
Sehyeon Youn <dev.younlab@gmail.com>
2018-12-04 20:48:57 +0900

    [ADD] exemple-03 if else, else if

--- nomad-coders-vanillaJavascript/exemple-02/index.js
+++ nomad-coders-vanillaJavascript/exemple-02/index.js
@@ -7,3 +7,29 @@ title.style.color = "red";
 // document 로 config 조작
 console.dir(document);
 document.title = "I own in owner";
+
+// function handleResize() {
+//   console.log("I have been resized");
+// }
+
+const handleResize = event => {
+  console.log(event);
+};
+
+// 화면 사이즈가 변경될때 해당하는 함수가 실행된다.
+window.addEventListener("resize", handleResize);
+title.style.cursor = "pointer";
+const handleClick = event => {
+  if (title.style.color === "red") {
+    title.style.color = "blue";
+  } else if (title.style.color === "blue") {
+    title.style.color = "red";
+  }
+};
+title.addEventListener("click", handleClick);
+
+if (condition) {
+  block;
+} else {
+  block;
+}
---
+++ nomad-coders-vanillaJavascript/exemple-03/index.js
@@ -0,0 +1,11 @@
+const age = prompt("How old are you");
+
+console.log(age);
+
+if (age >= 18 && age <= 21) {
+  console.log("you can drink");
+} else if (age > 21) {
+  console.log("go ahed");
+} else {
+  console.log("to yang");
+}
