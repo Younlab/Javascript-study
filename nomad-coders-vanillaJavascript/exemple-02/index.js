4807fbe740d485d1ee82a312a174424f50ba5b5e
Sehyeon Youn <dev.younlab@gmail.com>
2018-12-04 20:07:34 +0900

    [UPDATE] document 요소, dom selector

--- nomad-coders-vanillaJavascript/exemple-02/index.js
+++ nomad-coders-vanillaJavascript/exemple-02/index.js
@@ -1,4 +1,9 @@
-const title = document.getElementById("title");
-console.log(title);
+const title = document.querySelector("#title");
+console.dir(title);
 
 title.innerHTML = "Hi From JS";
+title.style.color = "red";
+
+// document 로 config 조작
+console.dir(document);
+document.title = "I own in owner";
