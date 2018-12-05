9619f6d142d9a156c600c415089b6763a68ab3c3
Sehyeon Youn <dev.younlab@gmail.com>
2018-12-06 00:16:11 +0900

    [UPDATE] localstorage 제어, 추가, 삭제

--- nomad-coders-vanillaJavascript/exemple-04/gretting.js
+++ nomad-coders-vanillaJavascript/exemple-04/gretting.js
@@ -1,24 +1,55 @@
 const form = document.querySelector(".js-form"),
   input = document.querySelector(".js-name-input"),
-  greeting = document.querySelector(".js-greetings");
+  greeting = document.querySelector(".js-greetings"),
+  deleteBtn = document.querySelector(".js-name-delete-btn");
 
 const USER_LS = "hello",
   SHOWING_CN = "showing";
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+  form.classList.remove(SHOWING_CN);
+  deleteBtn.classList.add(SHOWING_CN);
+}
 
 function paintGreeting(text) {
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `Hello ${text}`;
 }
 
+function handleNameDelete(event) {
+  event.preventDefault();
+  localStorage.removeItem(USER_LS);
+}
+
+function deleteName() {
+  deleteBtn.classList.add(SHOWING_CN);
+  deleteBtn.addEventListener("click", handleNameDelete);
+}
+
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   console.log("start");
   if (currentUser === null) {
-    // 유저가 있는 경우
+    askForName();
     console.log("false");
   } else {
     // 유저가 없는 경우
     paintGreeting(currentUser);
+
+    deleteName();
   }
 }
 
