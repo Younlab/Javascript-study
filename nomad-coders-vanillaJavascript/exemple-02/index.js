6ebd55de5402fa47689fac44483384054724e129
Sehyeon Youn <dev.younlab@gmail.com>
2018-12-05 14:04:31 +0900

    [UPDATE] classList event, add, remove, if else

--- nomad-coders-vanillaJavascript/exemple-02/index.js
+++ nomad-coders-vanillaJavascript/exemple-02/index.js
@@ -1,51 +1,26 @@
 const title = document.querySelector("#title");
-// console.dir(title);
 
-// title.innerHTML = "Hi From JS";
-// title.style.color = "red";
+const CLICKED_CLASS = "clicked";
 
-// document 로 config 조작
-// console.dir(document);
-// document.title = "I own in owner";
+// function handleClick() {
+//   // class 가 있는지 확인
+//   const hasClass = title.classList.contains(CLICKED_CLASS);
 
-// function handleResize() {
-//   console.log("I have been resized");
+//   // true flase 반환
+//   if (!hasClass) {
+//     title.classList.add(CLICKED_CLASS);
+//   } else {
+//     title.classList.remove(CLICKED_CLASS);
+//   }
 // }
 
-// const handleResize = event => {
-//   console.log(event);
-// };
+function handleClick() {
+  // 없으면 add 있으면 remove
+  title.classList.toggle(CLICKED_CLASS);
+}
 
-// 화면 사이즈가 변경될때 해당하는 함수가 실행된다.
-// window.addEventListener("resize", handleResize);
-// title.style.cursor = "pointer";
-
-// const handleClick = event => {
-//   if (title.style.color === "red") {
-//     title.style.color = "blue";
-//   } else if (title.style.color === "blue") {
-//     title.style.color = "red";
-//   }
-// };
-
-const BASE_COLOR = "red";
-
-const OTHER_COLOR = "#34495E";
-
-title.style.color = BASE_COLOR;
 function init() {
-  title.style.color = BASE_COLOR;
   title.addEventListener("click", handleClick);
 }
 
-function handleClick() {
-  const currentColor = title.style.color;
-  console.log(currentColor);
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
-}
-
-title.addEventListener("click", handleClick);
+init();
