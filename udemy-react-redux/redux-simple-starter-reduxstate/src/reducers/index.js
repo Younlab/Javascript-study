babd799d90f6b6958aff38df829a72aa5685a878
Sehyeon Youn <dev.younlab@gmail.com>
2018-12-13 01:25:05 +0900

    [UPDATE] reducer 에 actions 로 state value update, 값이 없을때의 예외처리

--- udemy-react-redux/redux-simple-starter-reduxstate/src/reducers/index.js
+++ udemy-react-redux/redux-simple-starter-reduxstate/src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
-
+import ActiveBook from "./reducer_active_book";
 import booksReducer from "./reducer_books";
 
 const rootReducer = combineReducers({
-  books: booksReducer
+  books: booksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
