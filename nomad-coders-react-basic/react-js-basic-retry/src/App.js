e0d840fa7004c9c75c500f6f8bad3360d2bcc5b6
Sehyeon Youn <dev.younlab@gmail.com>
2018-12-17 02:12:44 +0900

    [ADD] nomard-coder react basic retry

---
+++ nomad-coders-react-basic/react-js-basic-retry/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Movie from "./components/Movie";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Movie />
+      </div>
+    );
+  }
+}
+
+export default App;
