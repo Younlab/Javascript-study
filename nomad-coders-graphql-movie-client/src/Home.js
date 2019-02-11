7e49ac5474b08c662a8e23712add4a3f67c75479
Sehyeon Youn <dev.younlab@gmail.com>
2019-02-12 00:10:57 +0900

    [UPDATE] react-apollo query setting

--- nomad-coders-graphql-movie-client/src/Home.js
+++ nomad-coders-graphql-movie-client/src/Home.js
@@ -1,4 +1,22 @@
 import React from "react";
-
-const Home = () => <div>Home</div>;
+import { Query } from "react-apollo";
+import { HOME_PAGE } from "./queries";
+const Home = () => (
+  <Query query={HOME_PAGE}>
+    {({ loading, data, error }) => {
+      if (loading) return <span>loading</span>;
+      if (error) return <span>something happened</span>;
+      if (data) {
+        console.log(data);
+        return data.movies.map(movie => (
+          <div key={movie.id}>
+            <h3>{movie.title}</h3>
+            <h4>{movie.rating}</h4>
+            <img src={movie.medium_cover_image} alt={movie.title} />
+          </div>
+        ));
+      }
+    }}
+  </Query>
+);
 export default Home;
